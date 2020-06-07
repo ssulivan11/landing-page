31bdb433e7f4ade7151bf9e9cc540feb183fb83d
Scott Sullivan <ssulivan11@gmail.com>
2020-06-07 12:20:52 -0500

    better background

--- buildsize.config.js
+++ buildsize.config.js
@@ -2,12 +2,12 @@ module.exports = {
   files: [
     {
       path: 'public/build/bundle.js',
-      maxSize: '9kB',
+      maxSize: '9.8kB',
       compression: 'gzip'
     },
     {
       path: 'public/build/bundle.css',
-      maxSize: '1kB',
+      maxSize: '1.1kB',
       compression: 'gzip'
     }
   ]
