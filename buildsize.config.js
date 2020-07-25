d9e8a4ebc079b176301cdd79c9506ec3ea529378
Scott Sullivan <ssulivan11@gmail.com>
2020-07-25 15:57:22 -0500

    bundle size update

--- buildsize.config.js
+++ buildsize.config.js
@@ -2,13 +2,13 @@ module.exports = {
   files: [
     {
       path: 'public/build/bundle.js',
-      maxSize: '13.53kB',
+      maxSize: '13.83kB',
       lastSize: true,
       compression: 'gzip',
     },
     {
       path: 'public/build/bundle.css',
-      maxSize: '1.47kB',
+      maxSize: '1.48kB',
       lastSize: true,
       compression: 'gzip',
     },
